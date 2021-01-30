317111df7b5b288208760f7a8e89799634b95cd5
mael-25 <72653159+mael-25@users.noreply.github.com>
2021-01-30 16:34:05 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,9 @@
 alias "p"=python3
 
 p main.py\
-    --step=100\
-    --start_x=12\
-    --start_y=34\
-    --starting=2\
+    --step=50\
+    --start_x=25\
+    --start_y=25\
+    --starting=1\
     --name_p1="ask"\
     --name_p2="ask"\
